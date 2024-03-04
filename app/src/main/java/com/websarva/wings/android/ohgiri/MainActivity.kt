9dcb7627f0d82167d425cdb0fc1f077f625b6e54
Tera0108 <tenith0108@icloud.com>
2024-03-04 19:49:45 +0900

    例をたくさん渡した

--- app/src/main/java/com/websarva/wings/android/ohgiri/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/ohgiri/MainActivity.kt
@@ -105,7 +105,7 @@ class MainActivity : AppCompatActivity() {
             con.setRequestProperty("Content-type", "application/json")
             con.setRequestProperty(
                 "Authorization",
-                "Bearer API_KEY"
+                "Bearer APIKEY"
             )
 
             val bodyData =
@@ -164,7 +164,7 @@ class MainActivity : AppCompatActivity() {
 
             Log.d("debug", "入力した回答" + answerString)
 
-            temperature = "0.3";
+            temperature = "0.1";
 
             messageData =
                 messageData + ",{\"role\": \"user\", \"content\":\"回答「" + answerString + "」を10点満点で採点してください。返答の形式は1から10の整数値で返して。 例「1」 \"}"
@@ -190,9 +190,9 @@ class MainActivity : AppCompatActivity() {
         override fun onClick(v: View?) {
             val output = findViewById<TextView>(R.id.chatgptAnswer)
 
-            temperature = "0.3";
+            temperature = "0.1";
 
-            messageData = messageData + ",{\"role\": \"user\", \"content\":\"大喜利のお題の「" + theme + "」の爆笑解答を1つ考えてください。20文字以内でお願いします。例えば、お題「魔法使いが覚えたけど、結局使わない魔法とは？」で、回答「半透明人間になれる」です。\"}"
+            messageData = messageData + ",{\"role\": \"user\", \"content\":\"大喜利のお題の「" + theme + "」のシュールな解答を1つ答えてください。15文字以内で解答だけ返してください。例1は、お題「魔法使いが覚えたけど、結局使わない魔法とは？」で、回答「半透明人間になれる」です。例2は、お題「あだ名が「ダイナミック」な医者 その理由とは？」で、回答「脈拍をビートという」です。例3は、お題「芸能界の『暗黙の了解』を教えて下さい」で、回答「ガチャピンは手洗い ムックは丸洗い」です。例4は、お題「次のエピソードに、尾ひれを付けて話して下さい 『100円拾った』」で、回答「アメリカを敵にまわすかも知れない だけど俺は 100円拾った」です。例5は、お題「銭湯で長年番台を進めていた中村吉男さんが引退することに その理由とは？」で、回答「グラフィックデザインのほうに興味が出てきた」です。例6は、お題「「牛乳は噛んで飲むといい」みたいな事を教えて下さい」で、回答「ロッキーPART5は観なくてもいい」です。例7は、お題「「ナポリタン」という言葉を使って相手を震え上がらせて下さい」で、回答「幸せな家族の元に届いた不吉な一通のナポリタン」です。例8は、お題「殴られても蹴られてもチェ・ホンマンが離さなかったものとは？」で、回答「ドロー4」です。例9は、お題「0円でできる超スーパー暇つぶしとは？」で、回答「赤の他人の墓参り」です。例10は、お題「初めてドラゴンを退治しに行くのですが、アドバイスをお願いします」で、回答「手みやげ持ってくのと持ってかないのでは、全然対応が違うよ」です。\"}"
 
             val urlFull = "https://api.openai.com/v1/chat/completions"
             val result = receiveChatGptResponse(urlFull)
